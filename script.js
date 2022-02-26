fd39855592c1049eed193b3615de2a758c4dca7d
Thaysa <thay.fernnanda@gmail.com>
2022-02-26 14:39:11 -0300

    adiciona requisito 15 16 17 18 19

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ window.onload = () => {
   buttonLogin.addEventListener('click', login);
 };
 
-// // chamei o botão e desabilitei
-// const botao = document.querySelector('#submit-btn');
+//  chamei o botão e desabilitei
+const botao = document.querySelector('#submit-btn');
 
-// botao.disabled = true;
+botao.disabled = true;
 
-// // chamei o chekbox e adicionei um evento de input, ao qual se estivesse desabolitado o botão não ativava
-// document.getElementById('agreement').addEventListener('input', () => {
-//   const ativar = document.getElementById('agreement').value;
-//   if (ativar !== null && ativar !== '') {
-//     document.getElementById('submit-btn').disabled = false;
-//   } else {
-//     document.getElementById('submit-btn').disabled = true;
-//   }
-// });
+// chamei o chekbox e adicionei um evento de input, ao qual se estivesse desabolitado o botão não ativava
+document.getElementById('agreement').addEventListener('input', () => {
+  const ativar = document.getElementById('agreement').value;
+  if (ativar !== null && ativar !== '') {
+    document.getElementById('submit-btn').disabled = false;
+  } else {
+    document.getElementById('submit-btn').disabled = true;
+  }
+});
