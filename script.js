758febd9c07e49a529d25aee9c8ab7c8f54ef9f7
Thaysa <thay.fernnanda@gmail.com>
2022-02-26 12:31:27 -0300

    adiciona radio button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+// chamei o botão e desabilitei
+const botao = document.querySelector('#submit-btn');
+
+botao.disabled = true;
+
+// chamei o chekbox e adicionei um evento de input, ao qual se estivesse desabolitado o botão não ativava
+document.getElementById('agreement').addEventListener('input', () => {
+  const ativar = document.getElementById('agreement').value;
+  if (ativar !== null && ativar !== '') {
+    document.getElementById('submit-btn').disabled = false;
+  } else {
+    document.getElementById('submit-btn').disabled = true;
+  }
+});
