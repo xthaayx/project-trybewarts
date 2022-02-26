afe0b97f180a0018df73016dcd28dea5259b2e66
lucasbarreto92 <lucasbs.code@gmail.com>
2022-02-26 00:20:50 -0300

    REQUISITOS 01 A 07

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
- 
+const email = document.querySelector('#emailLogin');
+const senha = document.querySelector('#senhaLogin');
+const buttonLogin = document.querySelector('#enter');
+
+// FUNÇÃO PARA LOGAR NO SITE
+
+function login() {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+window.onload = () => {
+  buttonLogin.addEventListener('click', login);
+};
